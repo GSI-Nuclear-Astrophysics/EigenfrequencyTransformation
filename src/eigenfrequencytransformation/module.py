3718f396b15b17949fb53ca63f5059fbbe0a1ed7
DFreireF <D.FreireFernandez@gsi.de>
2026-08-13 10:39:19 +0200

    Common PyPi strucure jue 13 ago 2026 10:39:19 CEST

---
+++ src/eigenfrequencytransformation/module.py
@@ -0,0 +1,3 @@
+# Define here all your functions, methods, classes, etc. 
+# Then you can load them in your notebook with something like:
+# from eigenfrequencytransformation import * or create another jupyter notebook and do %run NAME.ipynb in the beginning
